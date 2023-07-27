26fb6de8043342f332629ff84e9ca401744ac2a8
dominicmwikya <dominicmwikya50@gmail.com>
2023-07-27 20:54:06 +0300

    Authguard

--- src/Modules/Products/ProductModule.ts
+++ src/Modules/Products/ProductModule.ts
@@ -1,16 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule  } from "@nestjs/common";
 import { ProductService } from "./ProductService";
 import {ProductController} from './ProductController';
-import { verifyJWTtoken } from 'src/Middlwares/verifyJWTtoken';
 @Module({
  controllers:[ProductController],
  providers:[ProductService],
  exports:[ProductService]
 })
-export class ProductModule implements NestModule{
-    configure(consumer: MiddlewareConsumer) {
-        consumer
-           .apply(verifyJWTtoken)
-           .forRoutes('products');
-    }
-}
+export class ProductModule {}
