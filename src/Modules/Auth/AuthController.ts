bf81867c74ca446abc93f2cfaef3d036f3726451
dominicmwikya <dominicmwikya50@gmail.com>
2023-08-24 13:23:07 +0300

    Added Authguard to verify user token and send logged in user data on refresh: Endpoint verify-token

--- src/Modules/Auth/AuthController.ts
+++ src/Modules/Auth/AuthController.ts
@@ -1,41 +1,37 @@
 
+import { Body, Controller, Get, HttpStatus, Post, Request, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "./AuthService";
-import { Body, Post, Controller, HttpCode, HttpStatus, UseGuards, Get, Request, Res, HttpException } from "@nestjs/common";
-import { signInDTO } from './siginInDTO';
 import { AuthGuard } from "./authGuard";
-import { Roles } from "./role.decorator";
-import { Role } from "./role.enum";
-import { roleGuard } from "./rolesGuards";
+import { signInDTO } from './siginInDTO';
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) { }
+	constructor(private authService: AuthService) { }
 
-  @Post('login')
-  async signIn(@Body() data: signInDTO, @Res() response) {
-    try {
-      const result = await this.authService.Signin(data.email, data.password);
-      response.status(HttpStatus.OK).json(result);
-    } catch (error) {
-      response.status(HttpStatus.UNAUTHORIZED).json({
-        error: error,
-      }); // Send a generic error response for failed login
-    }
-  }
+	@Post('login')
+	async signIn(@Body() data: signInDTO, @Res() response) {
+		try {
+			const result = await this.authService.Signin(data.email, data.password);
+			response.status(HttpStatus.OK).json(result);
+		} catch (error) {
+			response.status(HttpStatus.UNAUTHORIZED).json({
+				error: error,
+			}); // Send a generic error response for failed login
+		}
+	}
 
-  @UseGuards(AuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
+	@UseGuards(AuthGuard)
+	@Get('verify-token')
+	GetUserTokenVerification(@Request() req: any) {
+		if (req.user) {
+			return {
+				id: req.user.id,
+				email: req.user.email,
+				authState: req.user.authState
+			}
+		}
+		return {
+			error: "No valid token found! please login"
+		}
+	}
 
-  // @UseGuards(roleGuard)
-  @UseGuards(AuthGuard)
-  @Post('admin')
-  @Roles(Role.Admin)
-  @UseGuards(roleGuard)
-  getProducts(@Body() data: signInDTO) {
-    return {
-      name: data.email
-    }
-  }
 }
